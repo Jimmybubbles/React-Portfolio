5f1e3d079e41036c981809cbb69c38f3f75046f2
james <james.anthony.russell36@gmail.com>
2023-01-14 15:46:35 +1100

    reproducing the layout of the site to incorperate tailwind and have a portfolio easily updated.

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,17 +1,27 @@
-import "./index.css"
-import React from 'react';
-import Header from './components/Header'
-import Footer from './components/Footer'
-
-
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contacts from './Pages/Contact';
+import Projects from './Pages/Projects';
+import Technologies from './Pages/Technologies';
 
 function App() {
     return (
-        <div>
-            <Header></Header>
-            <Footer></Footer>
-        </div>
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contacts />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/technologies" element={<Technologies />} />
+            </Routes>
+            <Footer />
+        </Router>
     );
 }
+
 
 export default App;
---
+++ portfolio/src/details.js
@@ -0,0 +1,114 @@
+// all the details to be linked to for the site
+// logo images
+import logogradient from './assets/logo.svg'
+import logo from './assets/logo2.svg'
+
+// profile image
+import profile from './assets/profile.jpg'
+
+// tech stack images
+import html from './assets/techstack/html.png'
+import css from './assets/techstack/css.png'
+import sass from './assets/techstack/sass.png'
+import js from './assets/techstack/js.png'
+import react from './assets/techstack/react.png'
+import tailwind from './assets/techstack/tailwind.png'
+import bootstrap from './assets/techstack/bootstrap.png'
+import vscode from './assets/techstack/vscode.png'
+import apollo from './assets/techstack/apollo.png'
+import node from './assets/techstack/node.png'
+import npm from './assets/techstack/npm.png'
+import insomnia from './assets/techstack/insomnia.png'
+
+// Project Images
+import projectImage1 from './assets/project/project1.jpg'
+import projectImage2 from './assets/project/project2.jpg'
+import projectImage3 from './assets/project/project3.jpg'
+import projectImage4 from './assets/project/project4.jpg'
+import projectImage5 from './assets/project/project5.jpg' 
+import projectImage6 from './assets/project/project6.jpg'
+
+// Logos
+export const logos = {
+    logogradient: logogradient,
+    logo: logo,
+}
+
+// Personal Details
+
+export const personalDetails = {
+    name: "James Russell",
+    tagline: "Create online presents for companies",
+    img: profile,
+    about: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book`
+};
+
+// social media urls
+export const socialMediaUrl = {
+    linkedin: "https://www.linkedin.com",
+    github: "https://www.github.com",
+    twitter: "https://www.twitter.com",
+    instagram: "https://www.instagram.com"
+}
+
+export const workDetails = [
+    {
+        Position: "Frontend Web Developer",
+        Company: "finance hub",
+        Location:"Australia",
+        Type:"Full time",
+        Duration:"Sep 2021 - Dec 2021",
+    },
+
+    {
+        Position: "Frontend Web Developer",
+        Company: "finance hub",
+        Location:"Australia",
+        Type:"Full time",
+        Duration:"Sep 2021 - Dec 2021",
+    },
+
+    {
+        Position: "Internship",
+        Company: "finance hub",
+        Location:"Australia",
+        Type:"Full time",
+        Duration:"Sep 2021 - Dec 2021",
+    }
+];
+
+// Education details
+export const eduDetails = [
+    {
+        Position: "Frontend Development",
+        Company: "Udemy, YouTube, Google, Medium",
+        Location: "Online",
+        Type: "Full Time",
+        Duration: "Jan 2022 - Present",
+    },
+    {
+        Position: "full stack bootcamp ",
+        Company: "Monash University",
+        Location: "Online",
+        Type: "Full Time",
+        Duration: "August 2022 - Present",
+    }
+];
+
+// dummy project details
+export const projectDetails = [
+{
+    title: "Project title 1",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+]
+
+// Contact Details
+export const contactDetails = {
+    email: 'james.anthony.russell36@gmail.com',
+}
