ebb31631d74893b9a0b38bb058a7b6f2f8977a7b
james <james.anthony.russell36@gmail.com>
2023-01-15 16:05:23 +1100

    added contact form through tailwind

--- portfolio/src/Pages/contact.js
+++ portfolio/src/Pages/contact.js
@@ -1,22 +1,79 @@
-import React from "react";
-import { contactDetails } from "../details";
 
-function Contact() {
-    const {email, discord} = contactDetails;
-    return(
-        <main className="container mx-auto max-width section">
-            <h1 className="text-center text-2xl md:text-3xl lg:text-6xl text-dark-heading dark:text-light-heading font-semibold md:font-bold">
-            For any questions email or add on discord.    
-            </h1>
-            <h3 className="text-center text-3x1 md:text-4xl lg:text-6xl text-gradient font-semibold md:font-bold pt-f md:pt-10 md:pb-6">
-                <a href={`mailto:${email}`}>{email}</a>
-            </h3>
-            <span className="text-center text-content text-xl font-light block">or</span>
-            <h3 className="text-center text-3xl md:text-4xl lg:text-6xl text-gradient font-semibold md:font-bold">
-                <a href={`tel:${discord}`}>{discord}</a>
-            </h3>
-        </main>
-    )
-}
+import React, { useState } from "react";
+
+const FORM_ENDPOINT = ""; // TODO - fill on the later step
+
+const Contact = () => {
+    const [submitted, setSubmitted] = useState(false);
+    const handleSubmit = () => {
+        setTimeout(() => {
+            setSubmitted(true);
+        }, 100);
+    };
+
+    if (submitted) {
+        return (
+            <>
+                <div className="text-2xl">Thank you!</div>
+                <div className="text-md">We'll be in touch soon.</div>
+            </>
+        );
+    }
+
+    return (
+        <form
+            className="container mx-auto max-width pt-10 pb-10"
+            action={FORM_ENDPOINT}
+            onSubmit={handleSubmit}
+            method="POST"
+            target="_blank"
+            id="contact-form"
+        >
+            <div className="container mx-auto max-width pt-10 pb-20">
+                <fieldset>
+                    <legend class="sr-only">Checkbox variants</legend>
+
+                    <div class="flex items-center mb-4">
+                        <input id="checkbox-2" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                        <label for="checkbox-2" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Are you a legend to work with?</label>
+                    </div>
+
+
+                    <div class="flex items-center mb-4">
+                        <input id="checkbox-2" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                        <label for="checkbox-2" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Do you go hard?</label>
+                    </div>
+
+                    <div class="flex items-center mb-4">
+                        <input id="checkbox-3" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                        <label for="checkbox-3" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">love memes?</label>
+                    </div>
+
+
+                </fieldset>
+
+
+                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+
+                <div className="form-group">
+
+
+                    <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                    <input type="email" id="email" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="youremail@youremailprovider.com"></input>
+
+                </div>
+                <div className="form-group">
+
+                    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+                    <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
+
+                </div>
+                <button type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">hit the red button</button>
+            </div>
+
+        </form>
+    );
+};
 
 export default Contact;
